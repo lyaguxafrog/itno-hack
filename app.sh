addccf0d24fa3717f45e86235ecf2b4f64a7627e
Makridenko Adrian <adrian.makridenko@proton.me>
2024-09-28 00:57:45 +0300

    [+] app deploy

---
+++ app.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+config_app() {
+    cp kernel/.env.example kernel/.env
+    cp kernel/config/local_settings.example kernel/config/local_settings.py
+}
+
+get_tls() {
+    wget https://acme-v02.api.letsencrypt.org/directory
+    mv directory letsencrypt
+    exit 0
+}
+
+
+start_deploy() {
+    docker-compose up -d --build nginx
+}
+
+case "$1" in
+    config)
+        config_app
+        ;;
+    ssl)
+        get_tls
+        ;;
+    deploy)
+        start_deploy
+        ;;
+    *)
+    exit 0
+esac
